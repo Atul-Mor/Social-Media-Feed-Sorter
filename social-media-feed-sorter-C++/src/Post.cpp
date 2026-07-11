7b30c828a06296e660fe1fb9d6da91ba4b486d0b
Atul-Mor <149132587+Atul-Mor@users.noreply.github.com>
2026-07-11 14:11:01 +0530

    Delete social-media-feed-sorter-C++ directory

--- social-media-feed-sorter-C++/src/Post.cpp
+++
@@ -1,5 +0,0 @@
-
-#include "Post.h"
-
-Post::Post(int id, std::string user, int likes, long time, int priority)
-    : postId(id), userId(user), likes(likes), timestamp(time), userPriority(priority) {}
